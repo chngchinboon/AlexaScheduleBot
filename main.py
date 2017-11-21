2da907112695f007f43eb0232b9d4eff801484af
chngchinboon <chinboon@outlook.com>
2017-11-21 12:05:15 +0800

    prep for pica

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from flask import Flask
 from flask_ask import Ask, statement, question, session, context, request, version
-import time
+
+import requests as RQ
+from datetime import datetime,timedelta,tzinfo
+import iso8601
+import pytz
+import re
 
 import sqlite3
 
@@ -14,22 +19,74 @@ def dict_factory(cursor, row):
         d[col[0]] = row[idx]
     return d
 
-def get_infofromdb(tablename):    
+_DATE_PATTERNS = {
+    # "today", "tomorrow", "november twenty-fifth": 2015-11-25
+    '^\d{4}-\d{2}-\d{2}$': ['%Y-%m-%d',1],
+    # "this week", "next week": 2015-W48
+    '^\d{4}-W\d{2}$': ['%Y-W%U-%w',7],
+    # "this weekend": 2015-W48-WE
+    '^\d{4}-W\d{2}-WE$': ['%Y-W%U-WE-%w',2],
+    # "this month": 2015-11
+    '^\d{4}-\d{2}$': ['%Y-%m',30],
+    # "next year": 2016
+    '^\d{4}$': ['%Y',365],
+}
+
+def parse_timeframe(timeframe):
+    #assumed input is a datetime.date object from flask-ask convert
+    #build a dict with two fields of isoformat start and end        
+    #timestart=datetime.now().isoformat()
+    #timeend=timeframe.isoformat()#may need to add timedelta(1)
+
+    # make so 'next decade' matches work against 'next year' regex
+    timeframe = re.sub('X$', '0', timeframe)
+    for re_pattern, format_pattern in list(_DATE_PATTERNS.items()):
+        if re.match(re_pattern, timeframe):
+            if '%U' in format_pattern[0]:
+                # http://stackoverflow.com/a/17087427/1163855
+                timeframe += '-0'
+            datestart=datetime.strptime(timeframe, format_pattern[0]).date()                
+            dateend=datestart+timedelta(format_pattern[1])                                    
+            daterange={'date_start':datestart.isoformat(),'date_end':dateend.isoformat()}
+            return daterange
+    return None
+
+def get_infofromdb(tablename,timeframe='Today'):    
     '''
     exampledate=time.localtime()
     exampledatestr=time.strftime('%a, %d %b %Y %H:%M:%S',exampledate)
     examplecomment='Dental checkup at TTSH'
     schedulelist=[{'Datetime':exampledatestr,'comments':examplecomment}]
     ''' 
+    if tablename!='PICA':    
+    #get from database #to be removed
+        db = sqlite3.connect('data/testdb')
+        db.row_factory = dict_factory
+        cursor = db.cursor()
+        cursor.execute('select * from '+ tablename +' where username = "user1"')
+        infolist=cursor.fetchall()  
+        db.close()
     
-    #get from database
-    db = sqlite3.connect('data/testdb')
-    db.row_factory = dict_factory
-    cursor = db.cursor()
-
-    cursor.execute('select * from '+ tablename +' where username = "user1"')
-    infolist=cursor.fetchall()  
-    db.close()
+    #retrieve data from PICA
+    else:                 
+        #build object
+        data_to_send={'daterange':parse_timeframe(timeframe),
+              'alexa_id':context.System.device.deviceId                            
+                }
+        print(data_to_send)
+        #Send request to PICA
+        #r = requests.post('http://httpbin.org/post', data = {'key':'value'})        
+        #r = RQ.post(url, json = data_to_send)
+        
+        #parse return data from PICA
+        #iso8601.parse_date()
+        #time of the appointment
+        #type of service (e.g. meal service)
+        #assigned healthcare worker
+        #comments (this is optional e.g. assigned careworker will be running late)
+        data_recieved = r.json()
+        
+        
     return infolist
 
 def get_msg_info():
@@ -55,14 +112,14 @@ def log_usage(deviceID,msg_info,usage_info):
     db.close() #might want to try using 'with' instead.
     
     
-def get_appointment_msg():
+def get_appointment_msg(timeframe):
     print('Getting Appointments')
     
     msg_info=get_msg_info()
     print(msg_info)
     
     tablename='appointment'    
-    appointmentslist=get_infofromdb(tablename)
+    appointmentslist=get_infofromdb(tablename,timeframe)
     '''
     id INTEGER PRIMARY KEY,
     username TEXT,
@@ -185,9 +242,10 @@ def start_skill():
     welcome_message='Hello there!'
     return question(welcome_message)
 
-@ask.intent('GetAppointments')
-def get_appointment():    
-    return statement(get_appointment_msg())
+@ask.intent('GetAppointments', default={'TimeFrame': 'today'}, convert={'TimeFrame': 'date'})
+def get_appointment(TimeFrame):
+    print(TimeFrame)    
+    return statement(get_appointment_msg(TimeFrame))
 
 @ask.intent('GetMedication')
 def get_medication():
